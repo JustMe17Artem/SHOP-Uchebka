135f906a25598e95238e54aa5e8a95b70e08860f
JustMe17Artem <sematkinartem6@gmail.com>
2022-03-31 14:13:02 +0300

    ado net installed, DB classes added

---
+++ Shop/Shop/DataBase/DB_Connection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataBase
+{
+    class DB_Connection
+    {
+    }
+}
---
+++ Shop/Shop/DataBase/DataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Shop.my_ado;
+
+namespace Shop.DataBase
+{
+    public class DataAccess
+    {
+
+    }
+}
